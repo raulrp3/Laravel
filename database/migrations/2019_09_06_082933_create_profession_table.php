4e882bd05a35f3ac7e9238e20d548f63df1d6791
raulrp3 <raul.ramirez@escuelaestech.es>
2019-09-06 12:23:56 +0200

    Creados seeders para usuarios y profesiones

--- database/migrations/2019_09_06_082933_create_profession_table.php
+++ database/migrations/2019_09_06_082933_create_profession_table.php
@@ -15,7 +15,7 @@ class CreateProfessionTable extends Migration
     {
         Schema::create('professions', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('title', 100);
+            $table->string('title', 100)->unique();
             $table->timestamps();
         });
     }
